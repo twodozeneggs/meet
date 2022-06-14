f4d12cce5ef6cff628120a9833d52fd5eb1d924b
twodozeneggs <simonjohnloik@gmail.com>
2022-06-14 16:00:10 -0500

    more 4.8 updates (servie worker not working as a service)
    ugh, why.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
