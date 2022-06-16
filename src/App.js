c78341d3968f548dd8d36b3dea7ab1216dd2db4f
twodozeneggs <simonjohnloik@gmail.com>
2022-06-16 11:37:54 -0500

    Chart Party
    4.9 updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { getEvents, extractLocations } from './api';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import EventGenre from './EventGenre';
 import './nprogress.css'; 
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 export const getAccessToken = async () => {
   
@@ -21,6 +24,7 @@ class App extends Component {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
+        // console.log(events);
         this.setState({ events, locations: extractLocations(events) });
       }
     });
@@ -40,11 +44,62 @@ class App extends Component {
     });
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length;
+      
+      const city = location.split(", ").shift()
+      return {city,number};
+    })
+    console.log(data);
+    return data;
+  };
+
   render() {
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberofEvents={numberOfEvents} 
+        />
+        <div className="data-vis-wrapper">
+        <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+        </div>
+        <h4>Events in each city</h4>
+
+        {/* /* <ResponsiveContainer height={400} >
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis
+              allowDecimals={false}
+              type="number"
+              dataKey="number"
+              name="number of events"
+            />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer> */ }
+        <EventList events={this.state.events} />
         <EventList events={this.state.events} />
       </div>
     );
---
+++ src/EventGenre.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+const EventGenre = ({ events }) => {
+  const [data, setData] = useState([]);
+
+  const COLORS = ['#204051', '#84A9AC', '#FABB51', '#3B6978', '#C74B50'];
+
+  useEffect(() => {
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter(({ summary }) =>
+          summary.split(" ").includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      console.log(data);
+      return data;
+    };
+
+    setData(() => getData());
+  }, [events]);3
+
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart width={300} height={300}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          innerRadius={60}
+          outerRadius={90}
+          fill="#8884d8"
+          dataKey="value"
+          label={({ name, percent }) => {
+            if (percent > 0) {
+              return `${name} ${(percent * 100).toFixed(0)}%`;
+            }
+          }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenre;
