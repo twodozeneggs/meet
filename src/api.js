e6b7598412e81c80ef194a935b03d7e192f4b67f
twodozeneggs <simonjohnloik@gmail.com>
2022-08-05 17:57:38 -0500

    register service workers

--- src/api.js
+++ src/api.js
@@ -80,7 +80,7 @@ export const getAccessToken = async () => {
           const results = await axios.get(
              "https://l1s1cbtnk8.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url" 
           );
-          console.log("==results==", results)
+          console.log("===results==", results)
           const { authUrl } = results.data;
           return (window.location.href = authUrl);
       }
@@ -91,15 +91,22 @@ export const getAccessToken = async () => {
 
 
 //new token
-const getToken = (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const { access_token } = fetch( "https://l1s1cbtnk8.execute-api.eu-central-1.amazonaws.com/dev/api/token/" + encodeCode)
-      .then((res) => {
-        return res.json();
-      })
-      .catch((error) => console.log(error));
+const getToken = async (code) => {
   
+  try {
+    const encodeCode = encodeURIComponent(code);
+
+    const response = await fetch(
+      "https://l1s1cbtnk8.execute-api.eu-central-1.amazonaws.com/dev/api/token/" + encodeCode
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const { access_token } = await response.json();
     access_token && localStorage.setItem("access_token", access_token);
     return access_token;
+  } catch (error) {
+    error.json();
+  }
   };
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
 
-// atatus.config('8a901936bec549418ddbdbe0ce4d2f31').install();
+atatus.config('8a901936bec549418ddbdbe0ce4d2f31').install();
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -20,7 +20,7 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
